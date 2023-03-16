b563ae6b5a314202764e789c2a31bdb891961d24
Ankit Kumar <ankit.kr0048@gmail.com>
2023-03-16 13:13:40 +0530

    added OOP - Q5

--- Javascript/OOP/Q4/app.js
+++ Javascript/OOP/Q4/app.js
@@ -4,11 +4,11 @@ class Shape {
   }
 
   calculatePerimeter() {
-    console.log(`Perimeter of a ${this.name}: `);
+    return 0;
   }
 
   calculateArea() {
-    console.log(`Area of a ${this.name}: `);
+    return 0;
   }
 }
 
@@ -18,11 +18,19 @@ class Circle extends Shape {
     this.radius = radius;
   }
 
+  calculatePerimeter() {
+    return 2 * 3.14 * this.radius;
+  }
+
+  calculateArea() {
+    return 3.14 * this.radius ** 2;
+  }
+
   displayCircleMeasurments() {
-    super.calculatePerimeter();
-    console.log((2 * 3.14 * this.radius).toFixed(2));
-    super.calculateArea();
-    console.log((3.14 * this.radius * this.radius).toFixed(2));
+    const perimeter = this.calculatePerimeter().toFixed(2);
+    const area = this.calculateArea().toFixed(2);
+    console.log(`Perimeter of a Circle: ${perimeter}`);
+    console.log(`Area of a Circle: ${area}`);
   }
 }
 
@@ -32,39 +40,65 @@ class Square extends Shape {
     this.side = side;
   }
 
+  calculatePerimeter() {
+    return 4 * this.side;
+  }
+
+  calculateArea() {
+    return this.side ** 2;
+  }
+
   displaySquareMeasurments() {
-    super.calculatePerimeter();
-    console.log(4 * this.side);
-    super.calculateArea();
-    console.log(this.side * this.side);
+    const perimeter = this.calculatePerimeter();
+    const area = this.calculateArea();
+    console.log(`Perimeter of a Square: ${perimeter}`);
+    console.log(`Area of a Square: ${area}`);
   }
 }
 
 class Triangle extends Shape {
-  constructor(name, side1, side2, side3, side4, base, height) {
+  constructor(name, side1, side2, side3, base, height) {
     super(name);
     this.side1 = side1;
-    this.side2 = side2;
     this.side3 = side3;
-    this.side4 = side4;
+    this.side2 = side2;
     this.base = base;
     this.height = height;
   }
 
+  calculatePerimeter() {
+    return this.side1 + this.side2 + this.side3;
+  }
+
+  calculateArea() {
+    return (this.base * this.height) / 2;
+  }
+
   displayTriangleMeasurments() {
-    super.calculatePerimeter();
-    console.log(1 * (this.side1 + this.side2 + this.side3 + this.side4));
-    super.calculateArea();
-    console.log(((this.base * this.height) / 2).toFixed(2));
+    const perimeter = this.calculatePerimeter();
+    const area = this.calculateArea();
+    console.log(`Perimeter of a Triangle: ${perimeter}`);
+    console.log(`Area of a Triangle: ${area}`);
   }
 }
 
 var fs = require("fs");
-var input = fs.readFileSync("input.txt").trim().split("\n");
-
-var circle = new Circle(...input[0].split(","));
-var square = new Square(...input[1].split(","));
-var triangle = new Triangle(...input[2].split(","));
+var input = fs.readFileSync("input.txt").toString().trim().split("\n");
+var circle = new Circle(
+  ...input[0]
+    .split(",")
+    .map((i) => (i = parseFloat(i) != NaN ? parseFloat(i) : i))
+);
+var square = new Square(
+  ...input[1]
+    .split(",")
+    .map((i) => (i = parseFloat(i) != NaN ? parseFloat(i) : i))
+);
+var triangle = new Triangle(
+  ...input[2]
+    .split(",")
+    .map((i) => (i = parseFloat(i) != NaN ? parseFloat(i) : i))
+);
 
 circle.displayCircleMeasurments();
 square.displaySquareMeasurments();
